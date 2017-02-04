c9acfee3c74c5fcb06efc8632d2068bbfaa5b6a9
bigreni <bigreni@gmail.com>
2017-02-04 17:26:42 -0600

    v1 fixes 2

--- www/js/admob.js
+++ www/js/admob.js
@@ -9,7 +9,7 @@
     var admobid = {};
     if (/(android)/i.test(navigator.userAgent)) {
         admobid = { // for Android
-            banner: 'ca-app-pub-1683858134373419/4287481881',
+            banner: 'ca-app-pub-1683858134373419/7033447889',
             interstitial:'ca-app-pub-1683858134373419/6232537882'
            //banner: 'ca-app-pub-3886850395157773/3411786244'
             //interstitial: 'ca-app-pub-9249695405712287/3301233156'
